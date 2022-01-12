7499f65d36a309160deede241d35f2d4bfb6cc92
Rajesh G <rajesh@unigps.in>
2022-01-12 17:48:40 +0530

    base

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into CUSTOMER_ENTITY values('eric','Canada','1970-01-10','ADKIM3241D','unigps','Eric');
+insert into CUSTOMER_ENTITY values('john','USA','2001-02-04','AFNIG8472A','unigps','John');
+insert into CUSTOMER_ENTITY values('ratan','Bangalore','1985-12-23','FRMAI8204U','unigps','Ratan');
